9f265d8760e1966993702604377706f750a94203
Crowdbotics <team@crowdbotics.com>
2023-12-01 09:36:49 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "newdockerchanger-44833",
+  url: "https://newdockerchanger-44833.botics.co",
+  api: "https://newdockerchanger-44833.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
